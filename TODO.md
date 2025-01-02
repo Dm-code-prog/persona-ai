c524ebf9f8cd1c18b1cf5aaf301a5f3daefa2d4d
Dm-code-prog <dmitrykozlov504@gmail.com>
2025-01-02 21:49:36 +0200

    Subtitles with word highlight

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+### 1. Cool subtitiles using moviepy
+### 2. New output script format with footages
+### 3. Using local footage library
+### 4. Accept output format with music and sound effects
--- scripts/generate_shorts/main.py
+++ scripts/generate_shorts/main.py
@@ -3,7 +3,7 @@ import os
 
 from dotenv import load_dotenv
 
-from scripts.generate_shorts.script_prompt import prompt, get_script_prompt
+from scripts.generate_shorts.script_prompt import get_script_prompt
 from services.shorts_pipeline.ass import generate_ass_with_highlights, group_characters_into_words
 from services.shorts_pipeline.pipeline import ShortVideoPipeline
 
@@ -29,32 +29,32 @@ def main():
         pexels_api_key=pexels_api_key,
     )
 
-    topic = input("Enter the video topic")
-    prompt = get_script_prompt(topic)
+    # topic = input("Enter the video topic")
+    # prompt = get_script_prompt(topic)
 
     # timings_dict = json.load(open('output/eleven_labs_ts_dict.json'))
     # generate_ass_with_highlights(timings_dict, 'output/subtitles.ass')
 
-    pipeline.run(prompt)
+    # pipeline.run(prompt)
 
-    # pipeline.create_video(
-    #     '/Users/kozlovdmitriy/dev/persona_ai_project/persona_ai/scripts/generate_shorts/output/elevenlabs_script.mp3',
-    #     '/Users/kozlovdmitriy/dev/persona_ai_project/persona_ai/scripts/generate_shorts/output/subtitles.ass',
-    #     [
-    #         'output/pexels_footage_bodyweight exercises_0.mp4',
-    #         'output/pexels_footage_bodyweight exercises_1.mp4',
-    #         'output/pexels_footage_bodyweight exercises_2.mp4',
-    #         'output/pexels_footage_bodyweight exercises_3.mp4',
-    #         'output/pexels_footage_mental toughness_0.mp4',
-    #         'output/pexels_footage_mental toughness_1.mp4',
-    #         'output/pexels_footage_mental toughness_2.mp4',
-    #         'output/pexels_footage_mental toughness_3.mp4',
-    #         'output/pexels_footage_Navy SEAL training_0.mp4',
-    #         'output/pexels_footage_Navy SEAL training_1.mp4',
-    #         'output/pexels_footage_Navy SEAL training_2.mp4',
-    #         'output/pexels_footage_Navy SEAL training_3.mp4',
-    #     ]
-    # )
+    pipeline.create_video(
+        '/Users/kozlovdmitriy/dev/persona_ai_project/persona_ai/scripts/generate_shorts/output/elevenlabs_script.mp3',
+        '/Users/kozlovdmitriy/dev/persona_ai_project/persona_ai/scripts/generate_shorts/output/subtitles.ass',
+        [
+            'output/pexels_footage_bodyweight exercises_0.mp4',
+            'output/pexels_footage_bodyweight exercises_1.mp4',
+            'output/pexels_footage_bodyweight exercises_2.mp4',
+            'output/pexels_footage_bodyweight exercises_3.mp4',
+            'output/pexels_footage_mental toughness_0.mp4',
+            'output/pexels_footage_mental toughness_1.mp4',
+            'output/pexels_footage_mental toughness_2.mp4',
+            'output/pexels_footage_mental toughness_3.mp4',
+            'output/pexels_footage_Navy SEAL training_0.mp4',
+            'output/pexels_footage_Navy SEAL training_1.mp4',
+            'output/pexels_footage_Navy SEAL training_2.mp4',
+            'output/pexels_footage_Navy SEAL training_3.mp4',
+        ]
+    )
 
 
 if __name__ == "__main__":
